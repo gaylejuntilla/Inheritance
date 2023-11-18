234b1f515fef2787073a98c24e0b9b46bc67ec0f
gaylejuntilla <gayle.juntilla@my.jcu.edu.au>
2023-11-18 11:58:21 +1000

    Update band and musician classes

--- band.py
+++ band.py
@@ -1,5 +1,5 @@
 """
-Band class
+Band class with Hsu and Gayle
 """
 
 
@@ -8,6 +8,20 @@ class Band:
     def __init__(self, name=""):
         """"""
         self.name = name
-        self.musicans = []
+        self.musicians = []
 
-    def
+    def __repr__(self):
+        return f"{self.name} ({','.join([str(musician) for musician in self.musicians])})"
+
+    # def __repr__(self):
+    #     """"""
+    #     return str(vars(self))
+
+    def add(self, musician):
+        """"""
+        self.musicians.append(musician)
+
+    def play(self):
+        """"""
+        for musician in self.musicians:
+            print(musician.play())
